125979ecbb90f347d1ff38487dd3917bba90a010
Alicia <garcia.alicia1990@gmail.com>
2023-06-16 13:43:41 -0700

    Refactored code to handle Immutable image outside of controller and routing.

--- src/main/kotlin/com/img_processor/imageProcessing/ManipulateImage.kt
+++ src/main/kotlin/com/img_processor/imageProcessing/ManipulateImage.kt
@@ -1,8 +1,9 @@
-package com.img_processor.ImgManipulators
+package com.img_processor.imageProcessing
 
 import com.sksamuel.scrimage.ImmutableImage
 import com.sksamuel.scrimage.angles.Degrees
 import com.sksamuel.scrimage.filter.GrayscaleFilter
+import com.sksamuel.scrimage.nio.JpegWriter
 
 
 /**
@@ -15,7 +16,10 @@ import com.sksamuel.scrimage.filter.GrayscaleFilter
  * to radians.
  */
 
-class ManipulateImage(val image: ImmutableImage) {
+class ManipulateImage(val image: ByteArray) {
+
+    val immutableImage = convertToImmutableImage(image)
+
     /**
      * Rotate the image using the given member variable [image]
      * and [degree]
@@ -23,10 +27,12 @@ class ManipulateImage(val image: ImmutableImage) {
      * Todo: update code for handing the rectangle around the image
      * Return the degree rotated [image]
      */
-    fun rotateImage(degree: Int): ImmutableImage {
+    fun rotateImage(degree: Int): ByteArray {
         //Check the value of degree to determine which direction to rotate
         return if (degree != 0) {
-            image.rotate(Degrees(degree))
+            convertToByteArray(
+                immutableImage.rotate(Degrees(degree))
+            )
         } else {
             image // return unaltered image if value == zero
         }
@@ -38,15 +44,21 @@ class ManipulateImage(val image: ImmutableImage) {
      *
      * Return the rotated [image]
      */
-    fun rotate90LeftOrRight(direction:String): ImmutableImage {
+    fun rotate90LeftOrRight(direction: String): ByteArray {
         // check the direction and rotate, else return the same image unchanged
         return when (direction.lowercase()) {
             "right" -> {
-                image.rotateRight()
+                convertToByteArray(
+                    immutableImage.rotateRight()
+                )
             }
+
             "left" -> {
-                image.rotateLeft()
+                convertToByteArray(
+                    immutableImage.rotateLeft()
+                )
             }
+
             else -> {
                 image
             }
@@ -59,8 +71,10 @@ class ManipulateImage(val image: ImmutableImage) {
      *
      * Return the filtered [image]
      */
-    fun convertToGrayscale(): ImmutableImage {
-        return image.filter(GrayscaleFilter())
+    fun convertToGrayscale(): ByteArray {
+        return convertToByteArray(
+            immutableImage.filter(GrayscaleFilter())
+        )
     }
 
     /**
@@ -70,8 +84,10 @@ class ManipulateImage(val image: ImmutableImage) {
      *
      * Return the scaled [image]
      */
-    fun resizeImage(width:Int, height:Int): ImmutableImage {
-        return image.scaleTo(width, height)
+    fun resizeImage(width: Int, height: Int): ByteArray {
+        return convertToByteArray(
+            immutableImage.scaleTo(width, height)
+        )
     }
 
     /**
@@ -80,8 +96,10 @@ class ManipulateImage(val image: ImmutableImage) {
      *
      * Return the scaled [image]
      */
-    fun resizeImageWidth(width:Int): ImmutableImage {
-        return image.scaleToWidth(width)
+    fun resizeImageWidth(width: Int): ByteArray {
+        return convertToByteArray(
+            immutableImage.scaleToWidth(width)
+        )
     }
 
     /**
@@ -90,8 +108,10 @@ class ManipulateImage(val image: ImmutableImage) {
      *
      * Return the scaled [image]
      */
-    fun resizeImageHeight(height:Int): ImmutableImage {
-        return image.scaleToHeight(height)
+    fun resizeImageHeight(height: Int): ByteArray {
+        return convertToByteArray(
+            immutableImage.scaleToHeight(height)
+        )
     }
 
     /**
@@ -99,11 +119,13 @@ class ManipulateImage(val image: ImmutableImage) {
      *
      * Return the thumbnail [image]
      */
-    fun resizeImageToThumbnail(): ImmutableImage {
+    fun resizeImageToThumbnail(): ByteArray {
         val width = 160
         val height = 108
 
-        return image.scaleTo(width, height)
+        return convertToByteArray(
+            immutableImage.scaleTo(width, height)
+        )
     }
 
     /**
@@ -112,18 +134,41 @@ class ManipulateImage(val image: ImmutableImage) {
      *
      * Return the flipped [image]
      */
-    fun flipImage(direction:String): ImmutableImage {
+    fun flipImage(direction: String): ByteArray {
         // return the image flipped, else return same image unchanged
         return when (direction.lowercase()) {
             "horizontal" -> {
-                image.flipX()
+                convertToByteArray(
+                    immutableImage.flipX()
+                )
             }
+
             "vertical" -> {
-                image.flipY()
+                convertToByteArray(
+                    immutableImage.flipY()
+                )
             }
+
             else -> {
                 image
             }
         }
+    }
+
+    /**
+     * Helper function to convert [image] to ImmutableImage
+     */
+    private fun convertToImmutableImage(image: ByteArray): ImmutableImage {
+        // return an immutable image, only if it's a valid image
+        return ImmutableImage.loader().fromBytes(image)
+    }
+
+    /**
+     * Helper function to convert [image] to a bytearray
+     *
+     * Returns a ByteArray
+     */
+    private fun convertToByteArray(image: ImmutableImage): ByteArray {
+        return image.bytes(JpegWriter.Default)
     }
 }
