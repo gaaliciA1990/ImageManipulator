8764f87032b5c334c75adf3bfadd1ccd9cbdb6cd
Alicia <garcia.alicia1990@gmail.com>
2023-07-14 17:38:47 -0700

    Updated test variable names and removed tests for Sketch library.

--- src/test/kotlin/com/imageprocessor/imageprocessing/ScrimageImageProcessingTests.kt
+++ src/test/kotlin/com/imageprocessor/imageprocessing/ScrimageImageProcessingTests.kt
@@ -14,16 +14,16 @@ import kotlin.test.BeforeTest
  */
 
 class ScrimageImageProcessingTests {
-    val test1 = "images/testImg1.png"
-    val test2 = "images/testImg2.png"
+    val image1 = "images/testImg1.png"
+    val image2 = "images/testImg2.png"
 
     lateinit var byteImg1: ByteArray
     lateinit var byteImg2: ByteArray
 
     @BeforeTest
     fun beforeEach() {
-        byteImg1 = Files.readAllBytes(Paths.get(test1))
-        byteImg2 = Files.readAllBytes(Paths.get(test2))
+        byteImg1 = Files.readAllBytes(Paths.get(image1))
+        byteImg2 = Files.readAllBytes(Paths.get(image2))
     }
 
     @AfterTest
